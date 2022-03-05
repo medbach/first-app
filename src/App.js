b99d96bed218b94d7c158ff2468451aa5c1acb8e
unknown <mohamedbachchaouch17@gmail.com>
2022-03-05 10:23:25 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Formall from './component/Formall';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1>Form :nous contacter</h1>
+
+      <Formall />
+
+    
+
+    </div>
+  );
+}
+
+export default App;
